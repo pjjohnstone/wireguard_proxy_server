bba03fe368f203c22256cd6880c0953ad0736aab
Patrick Johnstone <johnstonep@gmail.com>
2020-04-13 19:55:58 +0100

    Clone repo locally first

--- packer/scripts/pihole.sh
+++ packer/scripts/pihole.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir /etc/pihole
 
 cat <<EOT > /etc/pihole/setupVars.conf
@@ -7,15 +8,16 @@ DNS_BOGUS_PRIV=true
 DNSSEC=false
 CONDITIONAL_FORWARDING=false
 PIHOLE_INTERFACE=wg0
-IPV4_ADDRESS=10.0.0.1/24
+IPV4_ADDRESS=10.0.0.1
 IPV6_ADDRESS=
 PIHOLE_DNS_1=127.0.0.1#5053
 PIHOLE_DNS_2=
 QUERY_LOGGING=false
-INSTALL_WEB_SERVER=false
-INSTALL_WEB_INTERFACE=false
-LIGHTTPD_ENABLED=false
+INSTALL_WEB_SERVER=true
+INSTALL_WEB_INTERFACE=true
+LIGHTTPD_ENABLED=true
 BLOCKING_ENABLED=true
 EOT
 
-curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
+git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
+bash "/etc/.pihole/automated install/basic-install.sh" --unattended
